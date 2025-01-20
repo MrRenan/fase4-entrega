d3adccfe8071addaea9ae2beb325e46bbfe795e6
Renan Leite <renan.carli@hotmail.com>
2025-01-19 21:00:09 -0300

    First Commit

---
+++ src/main/java/br/com/fiap/fase4entrega/Fase4EntregaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase4entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fase4EntregaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fase4EntregaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/fase4entrega/Fase4EntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase4entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fase4EntregaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
