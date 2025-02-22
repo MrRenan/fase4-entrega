e9b35920921f50a6733a70aaae56fb2355d358f3
Renan Leite <renan.carli@hotmail.com>
2025-02-21 21:43:41 -0300

    Corrigida configuração do rabbitmq

--- src/main/java/br/com/fiap/fase4entrega/infra/rabbitmq/PedidoConsumer.java
+++ src/main/java/br/com/fiap/fase4entrega/infra/rabbitmq/PedidoConsumer.java
@@ -3,7 +3,6 @@ package br.com.fiap.fase4entrega.infra.rabbitmq;
 import br.com.fiap.fase4entrega.features.adapter.out.EntregaAdapter;
 import br.com.fiap.fase4entrega.features.domain.entity.Entrega;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -13,15 +12,12 @@ import java.util.function.Consumer;
 @RequiredArgsConstructor
 public class PedidoConsumer {
 
-    private final StreamBridge streamBridge;
     private final EntregaAdapter adapter;
 
     @Bean
     public Consumer<String> pedidoPago() {
         return pedidoId -> {
-            // Lógica para processar o pedido pago
             System.out.println("Pedido recebido para entrega: " + pedidoId);
-            // Aqui você pode chamar o método que inicia o processo de entrega
             Entrega entrega = new Entrega(pedidoId);
             adapter.criarEntrega(entrega);
         };
